1f3b4968d40e39582b6e8e314ff228ab1a0c8ae7
Felipe Barros <felipetech@tuta.io>
2021-02-12 19:03:08 -0300

    feat: Secure request headers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const homeRoute = require('./routes/home')
-const secretRoute = require('./routes/secret')
-const cors = require('cors')
-const { join } = require('path')
+const express = require('express');
+const homeRoute = require('./routes/home');
+const secretRoute = require('./routes/secret');
+const cors = require('cors');
+const helmet = require('helmet');
+const { join } = require('path');
 
-require('./config/database')
+require('./config/database');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(homeRoute)
-app.use('/c31f41g0rluh3c4luc4p5z3t4cud1', secretRoute)
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(homeRoute);
+app.use('/c31f41g0rluh3c4luc4p5z3t4cud1', secretRoute);
 
-app.set('view engine', 'ejs')
-app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'));
 
-const PORT = process.env.PORT || 3000
-app.set('PORT', PORT)
+const PORT = process.env.PORT || 3000;
+app.set('PORT', PORT);
 
 app.listen(app.get('PORT'), () => {
-  console.log(`Server running on port ${app.get('PORT')}`)
-})
+  console.log(`Server running on port ${app.get('PORT')}`);
+});
